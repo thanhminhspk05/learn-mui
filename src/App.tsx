cdf6e09572483551e0b5898236f95ea77ffa0c3a
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-08-11 11:20:05 +0700

    save button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import MuiButton from './components/MuiButton';
+
+const defaultTheme = createTheme({
+  typography: {
+    button: {
+      textTransform: 'none',
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       {/* <MuiTypography /> */}
       <MuiButton />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -3,9 +3,36 @@ import { Button } from '@mui/material';
 function MuiButton() {
   return (
     <>
+      {/* TEXT */}
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button
+        variant="text"
+        color="error"
+      >
+        Text
+      </Button>
+
+      {/* CONTAINED */}
+      <Button
+        variant="contained"
+        color="error"
+      >
+        Contained
+      </Button>
+
+      {/* OUTLINED */}
+      <Button
+        variant="outlined"
+        disabled
+      >
+        OUTLINED
+      </Button>
+      <Button
+        variant="outlined"
+        color="error"
+      >
+        OUTLINED
+      </Button>
     </>
   );
 }
